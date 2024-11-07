08f7188db6783d5afe992a48473c9fd87df0edc3
Sazida Chowdhury <sanzidachowdhury81@gmail.com>
2024-11-07 20:49:24 +0600

    card content change

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Card } from 'react-bootstrap'
+
+import Card from './Card.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -6,6 +6,13 @@ const Card = () => {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <h2 className="text-3xl font-bold underline">
+      Hello world!
+    </h2>
+    <h3 className="text-3xl font-bold underline">
+      Hello world!
+    </h3>
+    
     </div>
   )
 }
